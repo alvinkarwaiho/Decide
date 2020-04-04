e35cb5f41958c52e9588e7ccaca49d27ef00dd17
alvin <alvinkarwaiho123@gmail.com>
2020-04-04 23:37:50 +0100

    update 1

--- app/src/main/java/com/example/decide/MainActivity.java
+++ app/src/main/java/com/example/decide/MainActivity.java
@@ -1,8 +1,13 @@
 package com.example.decide;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        BottomNavigationView botnavview = findViewById(R.id.navigation_bottom);
+        botnavview.setSelectedItemId(R.id.decisions);
+
+        botnavview.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+              switch(menuItem.getItemId()){
+                  case R.id.decisions:
+                      startActivity(new Intent(getApplicationContext(),Decision.class));
+                        return true;
+                  case R.id.home1:
+                      startActivity(new Intent(getApplicationContext(),Decision.class));
+                      return true;
+                  case R.id.notepad:
+                      startActivity(new Intent(getApplicationContext(),Decision.class));
+                      return true;
+              }
+                return false;
+            }
+        });
     }
 }
