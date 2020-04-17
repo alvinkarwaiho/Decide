81819b364f70f07a9e0eb95257857e719d754c94
alvin <alvinkarwaiho123@gmail.com>
2020-04-17 19:05:37 +0100

    Update of notepad with save and delete button

--- app/src/main/java/com/example/decide/Notepad.java
+++ app/src/main/java/com/example/decide/Notepad.java
@@ -46,8 +46,6 @@ public class Notepad extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
                 Save("theNotes.txt");
 
             }
